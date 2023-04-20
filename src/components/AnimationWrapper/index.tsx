b3ccb31d28e4fd6172c7a33de76fd889f6bd9915
xiaohai-huang <jeromehuang18@gmail.com>
2023-04-20 15:03:06 +0800

    keep old component alive in AnimationWrapper

--- src/components/AnimationWrapper/index.tsx
+++ src/components/AnimationWrapper/index.tsx
@@ -1,42 +1,25 @@
+import { type ReactElement, useEffect, useState } from "react";
 import { useTransition, animated } from "@react-spring/web";
-import { useState } from "react";
 import {
   NavigationType,
   useLocation,
   useNavigationType,
   useOutlet,
+  type Location,
 } from "react-router-dom";
 
 export default function AnimationWrapper() {
-  const location = useLocation();
-  const outlet = useOutlet();
-  const [elements, setElements] = useState(() => ({
-    [location.pathname]: outlet,
-  }));
-  const action = useNavigationType();
-  const pushConfig = {
-    from: { x: "100%", zIndex: 2 },
+  const stack = useStack();
+
+  const transitions = useTransition(stack, {
+    from: stack.length === 1 ? { x: "0%" } : { x: "100%" },
     enter: { x: "0%" },
     leave: {
-      x: "0.0001%",
-      zIndex: 1,
-    },
-  };
-
-  const popConfig = {
-    from: { x: "0%" },
-    enter: { x: "0%" },
-    leave: { x: "100%", zIndex: 2 },
-  };
-
-  const transitions = useTransition(location.pathname, {
-    ...(action === NavigationType.Push ? pushConfig : popConfig),
-    onStart: () => {
-      setElements((prev) => ({ ...prev, [location.pathname]: outlet }));
+      x: "100%",
     },
   });
 
-  return transitions((style, pathname) => (
+  return transitions((style, item) => (
     <animated.div
       style={{
         ...style,
@@ -46,7 +29,49 @@ export default function AnimationWrapper() {
         overflow: "hidden",
       }}
     >
-      {elements[pathname]}
+      {item.component}
     </animated.div>
   ));
+}
+
+type Stack = (Location & {
+  component: ReactElement | null;
+})[];
+function useStack() {
+  const location = useLocation();
+  const outlet = useOutlet();
+  const action = useNavigationType();
+  const [stack, setStack] = useState<Stack>([]);
+
+  useEffect(() => {
+    setStack((prev) => {
+      switch (action) {
+        case NavigationType.Push: {
+          return [...prev, { ...location, component: outlet }];
+        }
+        case NavigationType.Pop: {
+          // pop the top element to display the element under it
+          if (prev.length >= 2) {
+            if (prev.at(-2)?.key === location.key) {
+              // the user clicks the back button, pop the card at the top
+              return prev.slice(0, -1);
+            }
+          }
+          if (prev.find((item) => item.key === location.key) === undefined) {
+            // the page is first loaded
+            // or
+            // the user clicks the forward button
+            return [...prev, { ...location, component: outlet }];
+          }
+          return prev.slice(0, -1);
+        }
+        default: {
+          return prev;
+        }
+      }
+    });
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [location, action]);
+
+  return stack;
 }
