30cd152c14a26aa02dfa6a8a03a597ffad302d18
xiaohai-huang <jeromehuang18@gmail.com>
2023-04-20 19:38:04 +0800

    scroll if the page is too tall

--- src/components/AnimationWrapper/index.tsx
+++ src/components/AnimationWrapper/index.tsx
@@ -28,8 +28,10 @@ export default function AnimationWrapper() {
           style={{
             ...style,
             position: "absolute",
-            width: "100%",
-            height: "100%",
+            left: 0,
+            top: 0,
+            bottom: 0,
+            right: 0,
           }}
         >
           {item.component}
--- src/pages/animation/[id].tsx
+++ src/pages/animation/[id].tsx
@@ -22,6 +22,7 @@ function AnimationDetails() {
         backgroundColor: COLORS[Number(id)],
         width: "100%",
         height: "100%",
+        overflow: "scroll",
       }}
     >
       <h1 style={{ fontSize: "80px", textAlign: "center" }}>{id}</h1>
